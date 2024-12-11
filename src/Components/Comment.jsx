a1e82ff4968811974e39d9dc9a45f4678779ea0d
moisesVeSouza <mvs338409@hotmail.com>
2024-12-11 11:20:17 -0300

    Inclusão de funcionalidade de curtidas

--- src/Components/Comment.jsx
+++ src/Components/Comment.jsx
@@ -1,12 +1,21 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
+import { useState } from 'react';
 
 export function Comment({ content, onDeleteComment }) {
+    const [likeCount, setLikeCount] = useState(0)
 
     function handleDeleteComment() {
         onDeleteComment(content);
     }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1
+        })
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/moisesVeSouza.png" />
@@ -28,9 +37,9 @@ export function Comment({ content, onDeleteComment }) {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>28</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
